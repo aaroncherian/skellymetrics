488055212ac36cab7afc5c64e14222c9d38af2e3
aaroncherian <aaron.cherian2@gmail.com>
2024-01-12 11:45:36 -0500

    scatter plot updates

--- metric_comparison_dash_app/plotting/scatter_plot_3d.py
+++ metric_comparison_dash_app/plotting/scatter_plot_3d.py
@@ -1,21 +1,33 @@
 import plotly.express as px
 
+
 def create_3d_scatter_from_dataframe(dataframe_of_3d_data, ax_range=1200):
+    # Calculate mean values for setting axis ranges
     x_mean = dataframe_of_3d_data["x"].mean()
     y_mean = dataframe_of_3d_data["y"].mean()
     z_mean = dataframe_of_3d_data["z"].mean()
 
+    # Extract unique system names from the dataframe
+    system_names = dataframe_of_3d_data['system'].unique()
+
+    # Create a color map for the systems (modify this as needed)
+    colors = ['blue', 'orange', 'black', 'purple', 'orange', 'yellow']
+    color_discrete_map = {system: color for system, color in zip(system_names, colors)}
+
+    # Create the 3D scatter plot
     fig = px.scatter_3d(dataframe_of_3d_data, x="x", y="y", z="z", 
                         animation_frame="frame",
                         animation_group="marker",
                         color="system",
-                        color_discrete_map={'freemocap': 'blue', 'qualisys': 'red'},
+                        color_discrete_map=color_discrete_map,
                         hover_name="marker",
                         hover_data=["x", "y", "z"],
                         range_x=[x_mean - ax_range, x_mean + ax_range], 
                         range_y=[y_mean - ax_range, y_mean + ax_range],
-                        range_z=[z_mean - ax_range, z_mean + ax_range]
+                        range_z=[z_mean - ax_range, z_mean + ax_range],
     )
+
+    # Update the layout of the figure
     fig.update_layout(
         scene_aspectmode='cube',
         autosize=False,
@@ -24,6 +36,8 @@ def create_3d_scatter_from_dataframe(dataframe_of_3d_data, ax_range=1200):
         margin={"t": 1, "b": 1, "l": 1, "r": 1},
     )
 
+    # Adjust animation speed
     fig.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 1000 / 15
+    fig.update_traces(marker=dict(opacity=0.7))
 
     return fig
--- metric_comparison_dash_app/ui_components/scatter_plot_UI.py
+++ metric_comparison_dash_app/ui_components/scatter_plot_UI.py
@@ -1,5 +1,5 @@
-from dash_app.data_utils.sample_data import subsample_dataframe
-from dash_app.plotting.scatter_plot_3d import create_3d_scatter_from_dataframe
+from metric_comparison_dash_app.data_utils.sample_data import subsample_dataframe
+from metric_comparison_dash_app.plotting.scatter_plot_3d import create_3d_scatter_from_dataframe
 
 def create_3d_figure_from_subsampled_data(dataframe_of_3d_data, frame_skip_interval, color_of_cards):
     """Create marker figure with subsampled data"""
