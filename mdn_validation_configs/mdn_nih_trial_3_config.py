bb26d9c6ada9a21b93e0152f2fbc9ee2264403db
aaroncherian <aaron.cherian2@gmail.com>
2024-05-14 23:16:08 -0400

    Update mdn_nih_trial_3_config.py

--- mdn_validation_configs/mdn_nih_trial_3_config.py
+++ mdn_validation_configs/mdn_nih_trial_3_config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 from marker_sets.MDN_validation_marker_set import qualisys_nih_markers, markers_to_extract
-path_to_recording = Path(r"D:\2023-05-17_MDN_NIH_data\1.0_recordings\calib_3\mediapipe_MDN_Trial_3_yolo")
+path_to_recording = Path(r"D:\2023-05-17_MDN_NIH_data\1.0_recordings\calib_3\sesh_2023-05-17_14_53_48_MDN_NIH_Trial3")
 
 mdn_nih_trial_3_mediapipe_yolo_config = RecordingConfig(
     recording_name= 'MDN NIH Trial 3',
@@ -20,13 +20,14 @@ mdn_nih_trial_3_mediapipe_yolo_config = RecordingConfig(
 mdn_nih_trial_3_mediapipe_config = RecordingConfig(
     recording_name= 'MDN NIH Trial 3 Mediapipe',
     path_to_recording = path_to_recording,
-    path_to_freemocap_output_data = path_to_recording/'mediapipe_output_data'/'mediapipe_body_3d_xyz.npy',
-    path_to_qualisys_output_data = path_to_recording/'qualisys_data'/ 'qualisys_joint_centers_3d_xyz.npy',
+    path_to_freemocap_output_data = path_to_recording/'output_data'/'mediapipe_body_3d_xyz.npy',
+    path_to_qualisys_output_data = path_to_recording/'qualisys'/ 'clipped_qualisys_skel_3d.npy',
     qualisys_marker_list= qualisys_nih_markers,
     markers_to_compare_list= markers_to_extract,
     frame_for_comparison= 20,
     frame_range= None
 )
+
 
 
 if __name__ == '__main__':
@@ -37,5 +38,5 @@ if __name__ == '__main__':
     saved_transformation_matrix = None
     main(mdn_nih_trial_3_mediapipe_config,
         create_scatter_plot=False,
-        save_transformation_matrix=True,
+        save_transformation_matrix=False,
         transformation_matrix_to_use=saved_transformation_matrix)
